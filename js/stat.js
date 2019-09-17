d34818ce75b44210ed46b112b771869ed2ab3c70
Aleks <osinaleksandr1989@gmail.com>
2019-09-17 13:49:11 +0500

    создал функцию отрисовки столбцов-результатов и избавился от простыни

--- js/stat.js
+++ js/stat.js
@@ -11,19 +11,24 @@ var TOTAL_GAP = BAR_WIDTH + GAP;
 var BAR_HEIGHT = 150;
 var CLOUD_Y_SCORE = 70;
 
-
+// рисует облако для выведения результатов
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
-
+// пишет текст
 var renderText = function (ctx, text, x, y) {
   ctx.font = '16px PT Mono';
   ctx.fillStyle = 'black';
   ctx.textBaseline = 'hanging';
   ctx.fillText(text, x, y);
 };
-
+// рисует столбцы результов
+var renderBar = function (ctx, color, x, y, w, h) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, w, h);
+};
+// ищет максимальный элемент в массиве
 var getMaxElement = function (arr) {
   if (arr.length > 0) {
     var maxElement = arr[0];
@@ -49,14 +54,15 @@ window.renderStatistics = function (ctx, players, times) {
   var maxTime = getMaxElement(times);
   for (var i = 0; i < players.length; i++) {
     if (players[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillRect(CLOUD_X + TOTAL_GAP * i, CLOUD_Y_BAR, BAR_WIDTH, -BAR_HEIGHT * Math.round(times[i]) / Math.round(maxTime));
+      //рисуем красным столбец игрока (Вы)
+      renderBar(ctx, 'rgba(255, 0, 0, 1)', CLOUD_X + TOTAL_GAP * i, CLOUD_Y_BAR, BAR_WIDTH, -BAR_HEIGHT * Math.round(times[i]) / Math.round(maxTime));
     } else {
-      ctx.fillStyle = 'hsl(240, ' + Math.round(Math.random() * 100) + '%, 50%)';
-      ctx.fillRect(CLOUD_X + TOTAL_GAP * i, CLOUD_Y_BAR, BAR_WIDTH, -BAR_HEIGHT * Math.round(times[i]) / Math.round(maxTime));
+      //рисуем столбцы остальных игроков
+      renderBar(ctx,'hsl(240, ' + Math.round(Math.random() * 100) + '%, 50%)', CLOUD_X + TOTAL_GAP * i, CLOUD_Y_BAR, BAR_WIDTH, -BAR_HEIGHT * Math.round(times[i]) / Math.round(maxTime));
     }
-    ctx.fillStyle = 'black';
-    ctx.fillText(players[i], CLOUD_X + TOTAL_GAP * i, CLOUD_Y_TEXT);
-    ctx.fillText(Math.floor(times[i]), CLOUD_X + TOTAL_GAP * i, CLOUD_Y_SCORE + BAR_HEIGHT - (BAR_HEIGHT * Math.round(times[i]) / Math.round(maxTime)));
+    //пишем имена игроков снизу
+    renderText(ctx, players[i], CLOUD_X + TOTAL_GAP * i, CLOUD_Y_TEXT);
+    //пишем результаты сверху
+    renderText(ctx, Math.floor(times[i]), CLOUD_X + TOTAL_GAP * i, CLOUD_Y_SCORE + BAR_HEIGHT - (BAR_HEIGHT * Math.round(times[i]) / Math.round(maxTime)));
   }
 };
